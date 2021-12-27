e0283d006bee98571d943e97fdfc636bf6db1ccd
Shakhboz <shfazliddinov1940@gmail.com>
2021-12-27 10:19:47 +0500

    deployed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react'
+import './App.css'
+import {marked} from 'marked'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state={
+    markdown:""
+  }
+  // getMarkdownText() {
+  //   var rawMarkup = marked.parse("This is _Markdown_.");
+  //   return { __html: rawMarkup };
+  // }
+handleChange=(e)=>{
+  this.setState({
+    markdown:e.target.value
+  })
 }
 
+  render(){
+    const {markdown}=this.state;
+    const texts=marked(markdown);
+    return(
+    <div>
+        <h1 className="m-24 text-center ">Convert your markdown</h1>
+        <div className='row'>
+          <div className='col-md-6 '>
+            <textarea placeholder='Enter your markdown here' id="editor" className="form-control text" value={markdown} onChange={this.handleChange}  />
+          </div>
+          <div id="preview" className='col-md-6 '>
+            <div className="preview" dangerouslySetInnerHTML={{__html:texts}}  />
+          </div>
+        </div>
+     </div>
+    )
+  }
+}
 export default App;
